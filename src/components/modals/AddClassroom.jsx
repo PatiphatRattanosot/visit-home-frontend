6332ab57685c985bb664a7d7094ff8139dad8bb8
Punsano26 <xlichaphlreuxn@gmail.com>
2025-07-16 00:11:32 +0700

    Add maxLength to room and number fields in AddClassroom modal and update validation schema

--- src/components/modals/AddClassroom.jsx
+++ src/components/modals/AddClassroom.jsx
@@ -50,6 +50,7 @@ const AddClassroom = ({ addClassroomSuccess }) => {
                 type="number"
                 name="room"
                 placeholder="เลขชั้น"
+                maxLength={3}
                 disabled={false}
                 value={formik.values.room}
                 onChange={formik.handleChange}
@@ -64,6 +65,7 @@ const AddClassroom = ({ addClassroomSuccess }) => {
                 className="w-64 md:w-72"
                 label="ห้อง"
                 placeholder="เลขห้อง"
+                maxLength={3}
                 disabled={false}
                 value={formik.values.number}
                 onChange={formik.handleChange}
