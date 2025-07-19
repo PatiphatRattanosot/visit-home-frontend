bbd28825259897d9d563a4f2c6c3b1ab27ca4f3e
Punsano26 <xlichaphlreuxn@gmail.com>
2025-07-19 21:04:43 +0700

    Debug student update imports and API endpoints in StudentList and StudentService

--- src/services/student/student.service.js
+++ src/services/student/student.service.js
@@ -1,21 +1,21 @@
 import api from "../api";
 
-const baseUrl = "/users";
+const baseUrl = "/users/student";
 
 const getAllStudents = async () => {
-  return await api.get(`${baseUrl}/student`);
+  return await api.get(`${baseUrl}/students`);
 };
 
 const createStudent = async (data) => {
-  return await api.post(`${baseUrl}/student`, data);
+  return await api.post(`${baseUrl}/students`, data);
 };
 
 const getStudentById = async (id) => {
-  return await api.get(`${baseUrl}/student/${id}`);
+  return await api.get(`${baseUrl}/students/${id}`);
 };
 
 const updateStudent = async (id, data) => {
-  return await api.put(`${baseUrl}/student/${id}`, data);
+  return await api.put(`${baseUrl}/students/${id}`, data);
 };
 
 const StudentService = {
