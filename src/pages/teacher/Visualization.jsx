c75e72d9b70ae0df9c792e74df00a281fa102a9e
TanKittipat <kittipat12tan@gmail.com>
2025-09-30 21:28:20 +0700

    feat: Enhance Visualization component and add new visualiz service methods

--- src/pages/teacher/Visualization.jsx
+++ src/pages/teacher/Visualization.jsx
@@ -1,11 +1,31 @@
+import { Bar, Line, Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from "chart.js";
+import useYearSelectStore from "../../stores/year_select.store";
+import YearSelector from "../../components/YearSelector";
 
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement
+);
 
 const Visualization = () => {
   return (
     <div className="section-container">
       <h1>Visualization</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Visualization
+export default Visualization;
--- src/services/visualiz/visualiz.service.js
+++ src/services/visualiz/visualiz.service.js
@@ -9,10 +9,22 @@ const getVisualiz_total_status = async () => {
 const getVisualiz_total_visitor = async () => {
   return await api.get(`/show-visit-info-status`);
 };
+const getVisualiz_visit_status = async () => {
+  return await api.get(`/show-student-visit-status`);
+};
+const getVisualiz_sdq_status = async () => {
+  return await api.get(`/show-sdq-status`);
+};
+const getVisualiz_teacher_student_analytics = async () => {
+  return await api.get(`/teacher-student-analytics`);
+};
 
 const VisualizServices = {
   getVisualiz_total_role,
   getVisualiz_total_status,
   getVisualiz_total_visitor,
+  getVisualiz_visit_status,
+  getVisualiz_sdq_status,
+  getVisualiz_teacher_student_analytics,
 };
 export default VisualizServices;
